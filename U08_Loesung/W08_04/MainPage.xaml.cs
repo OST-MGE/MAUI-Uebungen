28888d976b5936ee965724c20921cdaa16cc4a04
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-07-01 10:28:22 +0200

    MAUI - U08: Lösung ergänzt

---
+++ U08_Loesung/W08_04/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace W08_04;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnHelloButtonClicked(object sender, EventArgs e)
+	{
+		OutputLabel.Text = $"Hello, {NameEntry.Text}!";
+	}
+}
+
