87ebdd56dddbf34bc8918fd0e0adeb47a27673e0
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-08-31 20:36:27 +0200

    .NET MAUI - U08: Aufgabe und Lösung überarbeitet.

---
+++ U08_Aufgabe/W08_03/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace W08_03;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ U08_Aufgabe/W08_03/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace W08_03;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
